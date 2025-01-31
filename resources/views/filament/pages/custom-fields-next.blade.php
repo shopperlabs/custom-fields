437a7526aa20b472003430fe68946b5b9f5841a6
manukminasyan <manuk.minasyan1@gmail.com>
2025-01-31 21:32:40 +0400

    Add custom fields component styles and Tailwind configuration

--- resources/views/filament/pages/custom-fields-next.blade.php
+++ resources/views/filament/pages/custom-fields-next.blade.php
@@ -1,26 +1,26 @@
 <x-filament-panels::page>
-    <x-filament::tabs label="Content tabs" contained>
-        @foreach ($this->entityTypes as $key => $label)
-            <x-filament::tabs.item active="{{ $key === $this->currentEntityType }}"
-                                   wire:click="setCurrentEntityType('{{ addslashes($key) }}')">
-                {{ $label }}
-            </x-filament::tabs.item>
-        @endforeach
-    </x-filament::tabs>
+    <div class="custom-fields-component">
+        <x-filament::tabs label="Content tabs" contained>
+            @foreach ($this->entityTypes as $key => $label)
+                <x-filament::tabs.item active="{{ $key === $this->currentEntityType }}"
+                                       wire:click="setCurrentEntityType('{{ addslashes($key) }}')">
+                    {{ $label }}
+                </x-filament::tabs.item>
+            @endforeach
+        </x-filament::tabs>
 
 
-    <div
-        x-sortable
-        wire:end.stop="updateSectionsOrder($event.target.sortable.toArray())"
-        class="flex flex-col gap-y-6"
-        x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('custom-fields', 'relaticle/custom-fields'))]"
-    >
-        @foreach ($this->sections as $section)
-            @livewire('manage-custom-field-section', ['entityType' => $this->currentEntityType, 'section' => $section, 'isDeletable' => $this->sections()->count() > 1], key($section->id . str()->random(16)))
-        @endforeach
+        <div
+            x-sortable
+            wire:end.stop="updateSectionsOrder($event.target.sortable.toArray())"
+            class="flex flex-col gap-y-6"
+            x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('custom-fields', 'relaticle/custom-fields'))]"
+        >
+            @foreach ($this->sections as $section)
+                @livewire('manage-custom-field-section', ['entityType' => $this->currentEntityType, 'section' => $section, 'isDeletable' => $this->sections()->count() > 1], key($section->id . str()->random(16)))
+            @endforeach
 
-        {{ $this->createSectionAction }}
+            {{ $this->createSectionAction }}
+        </div>
     </div>
-
-
 </x-filament-panels::page>
