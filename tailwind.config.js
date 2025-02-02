ede0a7f26b8c844a138ea433989105ead95ecfd7
manukminasyan <manuk.minasyan1@gmail.com>
2025-02-02 16:25:04 +0400

    Add Tailwind CSS configuration and update custom fields styles

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './resources/css/*.css',
+      './resources/views/**/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
