999faf6f0b80d6f6657ecbbef4fd4c96c15b5408
manukminasyan <manuk.minasyan1@gmail.com>
2025-02-04 18:08:53 +0400

    Add custom field model management methods to CustomFields class for Jetstream integration

--- src/CustomFields.php
+++ src/CustomFields.php
@@ -2,4 +2,50 @@
 
 namespace Relaticle\CustomFields;
 
-class CustomFields {}
+class CustomFields
+{
+
+    /**
+     * The custom field model that should be used by Jetstream.
+     *
+     * @var string
+     */
+    public static string $customFieldModel = 'Relaticle\\CustomFields\\Models\\CustomField';
+
+
+    /**
+     * Get the name of the custom field model used by the application.
+     *
+     * @return string
+     */
+    public static function customFieldModel(): string
+    {
+        return static::$customFieldModel;
+    }
+
+    /**
+     * Get a new instance of the custom field model.
+     *
+     * @return mixed
+     */
+    public static function newCustomFieldModel(): mixed
+    {
+        $model = static::customFieldModel();
+
+        return new $model;
+    }
+
+    /**
+     * Specify the custom field model that should be used by Jetstream.
+     *
+     * @param string $model
+     * @return static
+     */
+    public static function useCustomFieldModel(string $model): static
+    {
+        static::$customFieldModel = $model;
+
+        return new static;
+    }
+
+}
